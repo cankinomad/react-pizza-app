183b928947f70bd815f3fcd0b42b8685c8428c0f
Berk Aktas <aktasberk96@gmail.com>
2024-05-16 20:25:24 +0300

    first commit

---
+++ fast-react-pizza/src/features/cart/UpdateItemQuantity.jsx
@@ -0,0 +1,20 @@
+import { useDispatch, useSelector } from "react-redux"
+import Button from "../../ui/Button"
+import { decreaseItemQuantity, getCurrentQuantityById, increaseItemQuantity } from "./cartSlice";
+
+const UpdateItemQuantity = ({pizzaId }) => {
+    const dispatch= useDispatch();
+
+    const currentQuantity=useSelector(getCurrentQuantityById(pizzaId))
+
+    
+  return (
+    <div className="flex gap-1 items-center md:gap-3">
+        <Button type="round" onClick={()=>dispatch(decreaseItemQuantity(pizzaId))}>-</Button>
+        <p className="font-medium">{currentQuantity}</p>
+        <Button type="round" onClick={()=>dispatch(increaseItemQuantity(pizzaId))}>+</Button>
+    </div>
+  )
+}
+
+export default UpdateItemQuantity
---
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -0,0 +1,24 @@
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
+const AppLayout = () => {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
+  console.log(navigation);
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto] relative">
+      {isLoading && <Loader />}
+      <Header />
+      <div className="overflow-scrool ">
+      <main className="max-w-3xl mx-auto">
+        <Outlet />
+      </main>
+      </div>
+      <CartOverview />
+    </div>
+  );
+};
+
+export default AppLayout;
